d572194a00cfe68245c580f2029c500f8539be28
Jonathan Ternes <ternes.jonathangabriel@gmail.com>
2026-07-04 16:42:28 -0300

    fix: UserService

--- src/main/java/com/jonathandev/gestao_financeira/controllers/UserController.java
+++ src/main/java/com/jonathandev/gestao_financeira/controllers/UserController.java
@@ -2,49 +2,28 @@ package com.jonathandev.gestao_financeira.controllers;
 
 import com.jonathandev.gestao_financeira.dtos.UserRequestDto;
 import com.jonathandev.gestao_financeira.dtos.UserResponseDto;
-import com.jonathandev.gestao_financeira.service.UserService;
+import com.jonathandev.gestao_financeira.model.UserModel;
+import com.jonathandev.gestao_financeira.services.UserService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
-import java.util.UUID;
+
 
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
-    @PostMapping()
-    public ResponseEntity<UserResponseDto> criar(@RequestBody @Valid UserRequestDto userRequestDTO) {
-        UserResponseDto response = userService.criar(userRequestDTO);
+    @PostMapping("/cadastro")
+    public ResponseEntity<UserModel> novoUsuario(@RequestBody @Valid UserRequestDto userRequestDTO) {
+        UserModel response = userService.cadastrar(userRequestDTO);
+
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
-    }
-
-
-    @GetMapping("/...")
-    public ResponseEntity<List<UserResponseDto>> listarTodos() {
-        List<UserResponseDto> response = userService.listarTodos();
-        return ResponseEntity.ok(response);
-    }
-
-
-    @GetMapping("/{id}")
-    public ResponseEntity<UserResponseDto> buscarPorId(@PathVariable UUID id) {
-        UserResponseDto response = userService.buscarPorId(id);
-        return ResponseEntity.ok(response);
-    }
-
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity deletar(@PathVariable UUID id) {
-        userService.deletar(id);
-        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/com/jonathandev/gestao_financeira/services/UserService.java
+++ src/main/java/com/jonathandev/gestao_financeira/services/UserService.java
@@ -5,18 +5,15 @@ import com.jonathandev.gestao_financeira.exceptions.EmailFoundException;
 import com.jonathandev.gestao_financeira.model.UserModel;
 import com.jonathandev.gestao_financeira.model.UserRole;
 import com.jonathandev.gestao_financeira.repositories.UserRepository;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
-
-
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
 
     public UserModel cadastrar(UserRequestDto dto) {
         UserModel user = userRepository.findByEmail(dto.email());
