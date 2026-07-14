e5d1eda594ed3e9a583a30e20023dd55f0404298
Jonathan Ternes <ternes.jonathangabriel@gmail.com>
2026-07-14 20:52:24 -0300

    feat: Criando a classe que cria o token

---
+++ src/main/java/com/jonathandev/gestao_financeira/auth/TokenService.java
@@ -0,0 +1,38 @@
+package com.jonathandev.gestao_financeira.auth;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.jonathandev.gestao_financeira.model.UserModel;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String gerarToken(UserModel usuario){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT.create()
+                    .withIssuer("auth-api")
+                    .withSubject(usuario.getEmail())
+                    .withExpiresAt(gerarTempoDeExpirar())
+                    .sign(algorithm);
+            return token;
+
+        } catch (JWTCreationException e) {
+            throw new RuntimeException("Erro enquanto estava criando o token");
+        }
+    }
+
+    public Instant gerarTempoDeExpirar(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
