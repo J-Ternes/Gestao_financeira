6daab5f75973b8eaa99660bfa2c83be759bc8fb0
Jonathan Ternes <ternes.jonathangabriel@gmail.com>
2026-07-14 19:37:24 -0300

    feat: Inserindo AuthService

--- src/main/java/com/jonathandev/gestao_financeira/auth/AuthService.java
+++ src/main/java/com/jonathandev/gestao_financeira/auth/AuthService.java
@@ -1,4 +1,21 @@
 package com.jonathandev.gestao_financeira.auth;
 
-public class AuthService {
+import com.jonathandev.gestao_financeira.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    //Esse meetodo faz com que o Spring Security verifique o BD dos nossos usuários
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username);
+    }
 }
