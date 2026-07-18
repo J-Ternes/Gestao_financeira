5502bccbd3001aa4e4113faf83033c8624b2dbbd
Jonathan Ternes <ternes.jonathangabriel@gmail.com>
2026-07-18 12:02:22 -0300

    refactor: Concentrando todos os controllers na pasta controllers

--- src/main/java/com/jonathandev/gestao_financeira/controllers/UserController.java
+++ src/main/java/com/jonathandev/gestao_financeira/controllers/UserController.java
@@ -1,5 +1,10 @@
 package com.jonathandev.gestao_financeira.controllers;
 
+import com.jonathandev.gestao_financeira.auth.AuthRequestDto;
+import com.jonathandev.gestao_financeira.auth.AuthUserService;
+import com.jonathandev.gestao_financeira.auth.TokenService;
+import com.jonathandev.gestao_financeira.dtos.AuthRegisterDto;
+import com.jonathandev.gestao_financeira.dtos.AuthResponseDto;
 import com.jonathandev.gestao_financeira.dtos.UserRequestDto;
 import com.jonathandev.gestao_financeira.dtos.UserResponseDto;
 import com.jonathandev.gestao_financeira.model.UserModel;
@@ -9,6 +14,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,6 +28,31 @@ import java.util.UUID;
 public class UserController {
 
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final AuthUserService authUserService;
+    private final TokenService tokenService;
+
+
+
+
+    @PostMapping("/auth/registrar")
+    public ResponseEntity registrar(@RequestBody @Valid AuthRegisterDto dados){
+        authUserService.registrar(dados);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("Novo usuário criado com sucesso!");
+    }
+
+    @PostMapping("/auth/login")
+    public ResponseEntity login(@RequestBody @Valid AuthRequestDto dados){
+
+        UsernamePasswordAuthenticationToken emailPassword = new UsernamePasswordAuthenticationToken(dados.email(),dados.senha());
+
+        var auth = authenticationManager.authenticate(emailPassword);
+
+        var token = tokenService.gerarToken((UserModel) auth.getPrincipal());
+
+        return ResponseEntity.ok(new AuthResponseDto(token));
+    }
 
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/cadastrados")
