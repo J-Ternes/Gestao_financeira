5645b97de65106941e7af62a11a77e4e059b5c1d
Jonathan Ternes <ternes.jonathangabriel@gmail.com>
2026-07-23 17:49:24 -0300

    refactor: @NotBlanck no Dto Response Categoria

--- src/main/java/com/jonathandev/gestao_financeira/dtos/CategoriaResponseDto.java
+++ src/main/java/com/jonathandev/gestao_financeira/dtos/CategoriaResponseDto.java
@@ -1,6 +1,9 @@
 package com.jonathandev.gestao_financeira.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record CategoriaResponseDto(
+        @NotBlank (message = "Campo categoria vazio")
         String categoria
 ) {
 }
